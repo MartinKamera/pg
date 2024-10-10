d92b13b6d82f0e3c3ee426a2a64c8533d61109a7
MartinKamera <martin.kamera@seznam.cz>
2024-10-10 13:44:24 +0000

    cvico

--- second.py
+++ second.py
@@ -1,7 +1,35 @@
+#prevede cislo v intervalu 0 az 100 na jeho textovou podobu
 def cislo_text(cislo):
-    # funkce zkonvertuje cislo do jeho textove reprezentace
-    # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
-    return "dvacet pět"
+    cislo_cislem = int(cislo)
+    if cislo_cislem > 100:
+        return "Číslo je větší než sto"
+    if cislo_cislem < 0:
+        return "Číslo je menší než nula"    
+    else:
+
+        if cislo_cislem == 100:
+            cislo_slovy = "sto"
+        if cislo_cislem < 100:
+            
+            if (cislo_cislem >= 10 and cislo_cislem < 20):
+                nactky = ["deset","jedenáct","dvanáct","třináct","čtrnáct","patnáct","šestnáct","sedmnáct","osmnáct","devatenáct"]
+                cislo_slovy = nactky[int(cislo[1])]
+            else:
+                jednotky = ["nula","jedna","dva","tři","čtyři","pět","šest","sedm","osm","devět"]
+                desitky = ["dvacet","třicet","čtyřicet","padesát","šedesát","sedmdesát","osmdesát","devadesát"]
+                if cislo_cislem < 10:
+                    cislo_slovy = jednotky[int(cislo[0])]
+                else:
+                    if (cislo_cislem%10)!=0:
+                        cislo_slovy = desitky[int(cislo[0])-2] +" "+jednotky[int(cislo[1])]
+                    else:
+                        cislo_slovy = desitky[int(cislo[0])-2]
+        
+        return cislo_slovy
+            
+        
+        
+    
 
 if __name__ == "__main__":
     cislo = input("Zadej číslo: ")
