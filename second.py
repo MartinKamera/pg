bb25bdebdfb8f1bf16f75ce67bf93136b1fe9d61
MartinKamera <martin.kamera@seznam.cz>
2024-10-17 11:39:57 +0000

    cvico

--- second.py
+++ second.py
@@ -1,5 +1,6 @@
-#prevede cislo v intervalu 0 az 100 na jeho textovou podobu
+#prevede nmbstr v intervalu 0 az 100 na jeho textovou podobu
 def cislo_text(cislo):
+    nmbstr = str(cislo)
     cislo_cislem = int(cislo)
     if cislo_cislem > 100:
         return "Číslo je větší než sto"
@@ -13,17 +14,17 @@ def cislo_text(cislo):
             
             if (cislo_cislem >= 10 and cislo_cislem < 20):
                 nactky = ["deset","jedenáct","dvanáct","třináct","čtrnáct","patnáct","šestnáct","sedmnáct","osmnáct","devatenáct"]
-                cislo_slovy = nactky[int(cislo[1])]
+                cislo_slovy = nactky[int(nmbstr[1])]
             else:
                 jednotky = ["nula","jedna","dva","tři","čtyři","pět","šest","sedm","osm","devět"]
                 desitky = ["dvacet","třicet","čtyřicet","padesát","šedesát","sedmdesát","osmdesát","devadesát"]
                 if cislo_cislem < 10:
-                    cislo_slovy = jednotky[int(cislo[0])]
+                    cislo_slovy = jednotky[int(nmbstr[0])]
                 else:
                     if (cislo_cislem%10)!=0:
-                        cislo_slovy = desitky[int(cislo[0])-2] +" "+jednotky[int(cislo[1])]
+                        cislo_slovy = desitky[int(nmbstr[0])-2] +" "+jednotky[int(nmbstr[1])]
                     else:
-                        cislo_slovy = desitky[int(cislo[0])-2]
+                        cislo_slovy = desitky[int(nmbstr[0])-2]
         
         return cislo_slovy
             
