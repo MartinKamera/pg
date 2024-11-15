631d5b0a213088891df863a35ec6add718b8ec82
MartinKamera <martin.kamera@seznam.cz>
2024-11-15 22:04:51 +0000

    cvico

---
+++ sixth.py
@@ -0,0 +1,40 @@
+import sys
+import requests
+
+
+def download_url_and_get_all_hrefs(url):
+    req = requests.get(url)
+    
+    if req.status_code != 200:
+        print("Neplatné URL") 
+    
+    req_unicode = req.text
+    hrefs = []
+
+    i = 0
+    a = 0
+    
+    while i+5 <= len(req_unicode):
+        if (req_unicode[i:i+5] == "href="):
+            z = i + 6
+            href =[]
+            while req_unicode[z] !='"':
+                href.append(req_unicode[z])
+                z += 1
+
+            hrefs.append("".join(href))
+            i = z
+        else:
+            i += 1  
+    
+    print(hrefs)
+    return hrefs
+
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        download_url_and_get_all_hrefs(url)
+    # osetrete potencialni chyby pomoci vetve except
+    except Exception as e:
+        print(f"Program skoncil chybou: {e}")
